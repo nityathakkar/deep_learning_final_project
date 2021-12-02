df46d89450059d820f42af3473b1b7ddf24330dc
Nitya Thakkar <nitya_thakkar@brown.edu>
2021-12-02 18:51:24 -0500

    data preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,17 +5,22 @@ from sklearn.preprocessing import MinMaxScaler
 import heapq
 import stringdb
 import math
+from tqdm import tqdm
+from statistics import variance
 
 # Load csv file into pandas dataframes
 def load_data(path_exp, path_labels):
     exp_df = pd.read_csv(path_exp, index_col=0)
     labels_df = pd.read_csv(path_labels)
-    
+    print("Gene expression matrix shape: ", exp_df.shape)
     return exp_df, labels_df
 
 # TO DO ONCE HAVE REAL DATA!
 # remove unlabeled cells and cells labelled as debris and doublets
 # def filter_cells(exp_df, labels_df):
+    # assert len exp_df and len labels_df are the same
+    # if any of labels_df is debris or doublets, record index and remove
+    # use recorded index to remove those rows from exp_df
     # pass
     
 
@@ -37,10 +42,13 @@ def normalize(exp_df):
 # Sort the variances in descending order
 # Choose the top 1000 genes as the input of the classifiers
 def calc_variance(exp_df):
-    variance_dict = {}
 
-    for col_name in exp_df.columns:
-        variance_dict[col_name] = exp_df.var()[col_name]
+    var_values = exp_df.apply(variance, axis=0)
+    keys = exp_df.columns
+    variance_dict = dict(zip(keys, var_values))
+
+    # for col_name in tqdm(exp_df.columns):
+    #     variance_dict[col_name] = exp_df.var()[col_name]
 
     # Sort the dict in descending order and take top 1000 largest entries in dict
     top_1000 = heapq.nlargest(1000, variance_dict, key=variance_dict.get)
@@ -54,12 +62,14 @@ def create_adj_matrix(exp_df):
     # elements in matrix represent the confident score between pairs of genes extracted from the gene–gene interaction database (from StringDB)
     # Normalize weights by row sums
     # Use this to build a weighted graph where nodes are genes and edges represent the connection between genes and the normalized confidence scores are weights of edges
-    # genes = list(exp_df.columns.values)
-
-    genes = ['TP53', 'BRCA1', 'FANCD1', 'FANCL']
+    
+    
+    genes = list(exp_df.columns.values)
+    # print(genes)
+    
     string_ids = stringdb.get_string_ids(genes)
-    # print(string_ids.loc[:, 'taxonName'])
     gene_network = stringdb.get_network(string_ids.queryItem)
+    # gene_network.to_csv('network.csv')
 
     # TO CHECK LATER: WHY DUPLICATES CREATED?
     gene_network = gene_network.drop_duplicates()
@@ -67,35 +77,32 @@ def create_adj_matrix(exp_df):
     gene_network = gene_network.loc[:, ['preferredName_A', 'preferredName_B', 'score']]
     print(gene_network)
 
-    gene_network = np.asarray(gene_network)
-
-    for i in range(len(gene_network)):
-        if gene_network[i, 0] > gene_network[i, 1]:
-            gene_network[i, 0], gene_network[i, 1] = gene_network[i, 1], gene_network[i, 0]
-
-    gene_network_df = pd.DataFrame(gene_network, index=None, columns=['preferredName_A', 'preferredName_B', 'score'])
-
-    print(gene_network_df)
-    col_A = np.unique(gene_network_df.loc[:,'preferredName_A'])
-    col_B = np.unique(gene_network_df.loc[:,'preferredName_B'])
+    col_A = np.unique(gene_network.loc[:,'preferredName_A'])
+    col_B = np.unique(gene_network.loc[:,'preferredName_B'])
 
     genes = np.unique(np.concatenate([col_A, col_B]))  
     adj_matrix = pd.DataFrame(index=genes, columns=genes)
-
-    np.fill_diagonal(adj_matrix.values, 0) # Fill the diagonal with infinity
+    count = 0
+    np.fill_diagonal(adj_matrix.values, 0) # Fill the diagonal with 0 (we don't want self loops)
     for g1 in col_A:
-        match_1_df = gene_network_df.loc[gene_network_df['preferredName_A'] == g1]
-        for g2 in col_B:
+        match_1_df = gene_network.loc[gene_network['preferredName_A'] == g1]
+        # print(g1)
+        # print(match_1_df)
+        gene2_col = np.unique(match_1_df.loc[:,'preferredName_B'])
+        for g2 in gene2_col:
             if math.isnan(adj_matrix.loc[g1,g2]):
+                count +=1
+                # print(g2)
                 match_2_df = match_1_df.loc[match_1_df['preferredName_B'] == g2]
-                print(g1, g2)
-                print(len(match_2_df['score']))
+                # print(match_2_df)
+                # print(match_2_df['score'])
                 adj_matrix.loc[g1,g2] = float(match_2_df['score'])
                 adj_matrix.loc[g2, g1] = float(match_2_df['score'])
 
-    print(adj_matrix)
-
-    pass
+    adj_matrix = adj_matrix.fillna(0)
+    adj_matrix = adj_matrix.div(adj_matrix.sum(axis=1), axis=0)
+    print("filled #: ", count)
+    return adj_matrix
 
 
 if __name__ == '__main__':
@@ -117,9 +124,12 @@ if __name__ == '__main__':
     print("Calculating most variant genes...\n")
     var_df = calc_variance(normalize_df)
     # print(var_df)
+    # to_save = var_df.T
+    # to_save.to_csv('gene_exp.csv')
 
     print("Creating gene adjacency network")
     adj_matrix = create_adj_matrix(var_df)
+    print(adj_matrix)
 
 
 
